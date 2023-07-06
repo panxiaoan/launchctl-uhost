8f97615bc8796cd8280166865b0574b5f126aa74
ningbo.pan <xiaoan.pan@qq.com>
2023-07-06 09:55:47 +0800

    删除命令中的 sudo

--- uhost.sh
+++ uhost.sh
@@ -2,8 +2,8 @@
 
 echo `date` >> /tmp/uhost.log &&
 
-sudo sed -i "" "/# GitHub520 Host Start/,/# Github520 Host End/d" /etc/hosts && curl https://raw.hellogithub.com/hosts >> /etc/hosts
+sed -i "" "/# GitHub520 Host Start/,/# Github520 Host End/d" /etc/hosts && curl https://raw.hellogithub.com/hosts >> /etc/hosts
 
-sudo killall -HUP mDNSResponder
+killall -HUP mDNSResponder
 
 echo 'finish' >> /tmp/uhost.log
